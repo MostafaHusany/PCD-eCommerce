908c8e42ddd02ad0919a5b667eacda0590481c35
Mostafa El-Husany <mostafa.husany@gmail.com>
2022-03-21 04:22:20 +0200

    update brand action btns

--- resources/views/admin/brands/incs/_actions.blade.php
+++ resources/views/admin/brands/incs/_actions.blade.php
@@ -1,8 +1,4 @@
 <div class="text-center">
-    <button class="show-object btn btn-xs btn-info" data-object-id="{{$row_object->id}}">
-        <i class="fas fa-eye"></i>
-    </button>
-    
     <button class="edit-object btn btn-xs btn-warning" 
         data-object-id="{{$row_object->id}}"
         data-current-card="#objectsCard"    
