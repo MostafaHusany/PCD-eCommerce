ee6547ea36e19fbcb873d392cfe746f6e84cde4a
Mostafa El-Husany <mostafa.husany@gmail.com>
2022-03-29 19:19:15 +0200

    update style

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -40,6 +40,9 @@
 
     <body class="hold-transition sidebar-mini layout-fixed">
         <div class="wrapper">
+            <div class="preloader flex-column justify-content-center align-items-center">
+                <img class="animation__shake" src="{{ asset('images/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
+            </div>
             <!-- Main Header -->
             <nav class="main-header navbar navbar-expand navbar-white navbar-light">
                 <!-- Left navbar links -->
