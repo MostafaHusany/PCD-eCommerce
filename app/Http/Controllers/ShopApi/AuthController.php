b7ea71d61f2d5041782f63b9683bdab1208ae5fe
Mostafa El-Husany <mostafa.husany@gmail.com>
2022-07-10 18:03:47 +0200

    add invoice status to order

--- app/Http/Controllers/ShopApi/AuthController.php
+++ app/Http/Controllers/ShopApi/AuthController.php
@@ -265,7 +265,7 @@ class AuthController extends Controller
 
     public function getOrders (Request $request) {
 
-        $customerOrders = Order::query()->with(['products', 'invoice']);
+        $customerOrders = Order::query()->with(['products', 'invoice', 'status_obj']);
         $data = $customerOrders->where('customer_id', auth()->user()->customer->id)->paginate(15);
 
         return response()->json(array('data' => $data, 'success' => true));
