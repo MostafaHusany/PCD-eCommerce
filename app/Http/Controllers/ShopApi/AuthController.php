d570cb22e6bc392401138f70d2a6fb812af1626b
Mostafa El-Husany <mostafa.husany@gmail.com>
2022-07-10 14:29:20 +0200

    get order api, add the invoice to each order

--- app/Http/Controllers/ShopApi/AuthController.php
+++ app/Http/Controllers/ShopApi/AuthController.php
@@ -265,7 +265,7 @@ class AuthController extends Controller
 
     public function getOrders (Request $request) {
 
-        $customerOrders = Order::query()->with('products');
+        $customerOrders = Order::query()->with(['products', 'invoice']);
         $data = $customerOrders->where('customer_id', auth()->user()->customer->id)->paginate(15);
 
         return response()->json(array('data' => $data, 'success' => true));
