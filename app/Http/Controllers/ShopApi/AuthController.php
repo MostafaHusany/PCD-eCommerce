7e786260ef5b2e65d82f2590ef7e0f6e98fc515b
Mostafa El-Husany <mostafa.husany@gmail.com>
2022-08-08 14:17:29 +0200

    update auth api

--- app/Http/Controllers/ShopApi/AuthController.php
+++ app/Http/Controllers/ShopApi/AuthController.php
@@ -256,6 +256,7 @@ class AuthController extends Controller
         ];
 
         return response()->json([
+            'success'      => true,
             'customer'     => $customer,
             'access_token' => $token,
             'token_type'   => 'bearer',
