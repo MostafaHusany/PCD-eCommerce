33f8efe15c6d717be1915ee5e84c5dcbd3056711
Mostafa El-Husany <mostafa.husany@gmail.com>
2022-10-22 13:42:00 +0200

    smsSender urlencoding bug

--- app/Traits/SMSSender.php
+++ app/Traits/SMSSender.php
@@ -11,11 +11,11 @@ trait SMSSender {
     private $pass = 'password=pbdc741963';
 
     public function sendSms ($message, $phone_number) {
-        $message      = "message=$message";
+        $message      = "message=" . urlencode($message);
         $phone_number = "numbers=$phone_number";
-        $encode       = "sender=PCD%20SUPPORT&unicode=e&Rmduplicated=1&return=xml";
+        $encode       = "sender=PCD%20SUPPORT&unicode=e&Rmduplicated=1&return=json";
 
-        $url_sms  = $this->api_url . $this->user . '&' . $this->pass . '&' . urlencode($message) . '&' . $phone_number . '&' . $encode;
+        $url_sms  = $this->api_url . $this->user . '&' . $this->pass . '&' . $message . '&' . $phone_number . '&' . $encode;
         // $response_hmsms = file_get_contents($url_sms);
         $response = Http::get($url_sms);
     }
