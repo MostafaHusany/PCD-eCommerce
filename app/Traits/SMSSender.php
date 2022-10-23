7404d21b162fb82b45fc385a5d370726d5bd8668
Mostafa El-Husany <mostafa.husany@gmail.com>
2022-10-23 04:57:58 +0200

    Update sms trait

--- app/Traits/SMSSender.php
+++ app/Traits/SMSSender.php
@@ -5,14 +5,20 @@ namespace App\Traits;
 
 use Illuminate\Support\Facades\Http;
 
-use App\FailedMessage;
+use App\SentSMS;
 
 trait SMSSender {
     private $api_url = 'https://www.4jawaly.net/api/sendsms.php?';
     private $user = 'username=pcdoctor';
     private $pass = 'password=pbdc741963';
 
-    public function sendSms ($msg, $phone) {
+    public function sendSms ($msg, $phone, SentSMS $requested_sms = null) {
+
+        $requested_sms = isset($requested_sms) ? $requested_sms : SentSMS::create([
+            'phone'    => $phone,
+            'message'  => $msg
+        ]);
+
         $message      = "message=" . urlencode($msg);
         $phone_number = "numbers=$phone";
         $encode       = "sender=PCD%20SUPPORT&unicode=e&Rmduplicated=1&return=json";
@@ -20,6 +26,7 @@ trait SMSSender {
         $url_sms  = $this->api_url . $this->user . '&' . $this->pass . '&' . $message . '&' . $phone_number . '&' . $encode;
         $response = Http::get($url_sms);
         $response = (array) json_decode($response->body());
+    
 
         /*
             if the message not sent successfully 
@@ -27,12 +34,15 @@ trait SMSSender {
         */
         
         if ($response['Code'] != 100) {
-            FailedMessage::create([
-                'phone'    => $phone,
-                'message'  => $msg,
-                'err_code' => $response['Code'],
-                'err_msg'  => $response['MessageIs']
-            ]);
+            $requested_sms->status   = -1;
+            $requested_sms->err_code = $response['Code'];
+            $requested_sms->err_msg  = $response['MessageIs'];
+            $requested_sms->save();
+        } else {
+            $requested_sms->status = 1;
+            $requested_sms->save();
         }
+        
+        return $requested_sms->status == 1;
     }
 }
